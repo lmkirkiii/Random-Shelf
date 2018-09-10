1d324ca2af02b1dd310e359b6eeab62de65908e8
Lawrence Kirk <lmkirkiii@gmail.com>
2018-09-10 13:54:41 -0400

    added model

--- shelf_django/shelf/models.py
+++ shelf_django/shelf/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Treasure(models.Model):
+    title = models.CharField(max_length=1000)
+    description = models.CharField(max_length=1000)
+    img_url = models.TextField()
+
+    def __str__(self):
+        return self.name
